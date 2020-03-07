c37c5f567f7a8061ab67f17bc906302b098b3a2e
Prashant Singh <prashantsingh2408@gmail.com>
2020-03-07 14:05:46 +0530

    save image

--- registrationForm.py
+++ registrationForm.py
@@ -4,17 +4,24 @@ from time import sleep
 import os # for folder access
 
 #no of image of one person in database
-noOfImage = 1000
+noOfImage = 400
 webcam = cv2.VideoCapture(0)
-
+mainDir = os.getcwd()
 # give time to person to adject face.
 sleep(2)
 
 #start taking image
 while True:
-        #at staing input person name
-        if noOfImage == 1000:
+        #at staing 
+        if noOfImage == 400:
+            #Input name
             name = input("Enter name")
+            #Change dir to 'imgDataBase' where image store
+            os.chdir("imgDataBase")
+            #Create a directory(folder) with person name
+            os.mkdir(name)
+            #Return to main dir
+            os.chdir(mainDir)
         
         #Break while loop after taking 100 img
         noOfImage = noOfImage - 1
@@ -26,15 +33,25 @@ while True:
         # status  -> True if image taken otherwise False 
         check, frame = webcam.read()
         
-        #Create rectangle around face
+        #Detect face
             #converty to gray for good detection
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
             #detect face
         detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         faces = detector.detectMultiScale(gray,1.1,5)
-            #rectangle create
+        
+        #rectangle create and save img
         for (x,y,w,h) in faces:
             cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+            crop_face = gray[y:y+h, x:x+w]
+            #change dir
+            os.chdir("imgDataBase")
+            os.chdir(name)
+            #write img
+            imgName = "{0}{1}.jpg".format(name,noOfImage)
+            cv2.imwrite(imgName,crop_face)
+            #Return to main dir
+            os.chdir(mainDir)
             #update frame
         frame = gray
 
@@ -58,10 +75,6 @@ while True:
         #show the current img
         cv2.imshow("image", frame)
         key = cv2.waitKey(1)
-
-        #Create a directory(folder) with person name
-        
-        #save image in folder
 
         
 
