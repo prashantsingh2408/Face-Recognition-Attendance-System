1dadc4072bf7c26a0ca9c2bcdee5bec1385e5133
Prashant Singh <prashantsingh2408@gmail.com>
2020-03-09 16:48:16 +0530

    debug requires

--- takeAttendenceForm.py
+++ takeAttendenceForm.py
@@ -2,6 +2,7 @@
 from tkinter import filedialog
 from tkinter import *
 import cv2
+import time
 
 root = Tk()
 #It create file explorer to take img dir.
@@ -9,9 +10,9 @@ root.filename = filedialog.askopenfilename(
     initialdir = "/",
     title = "Select file",
     filetypes = (("jpeg files","*.jpg"),("all file","*.*"))
-    )
     
-print(root.filename)
-img = cv2.imread(root.filename)
-cv2.imshow("img",img)
+    )
+img = cv2.imread(root.filename)#Run time error,debug require
+cv2.imshow("img",img)#Run time error,debug require
+time.sleep(3)
 
