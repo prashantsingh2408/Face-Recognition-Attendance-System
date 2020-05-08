142588da9ef7c0864235f33c1a45c2d6af99df50
Prashant Singh <prashantsingh2408@gmail.com>
2020-05-08 16:41:08 +0530

    update recognizeFace

---
+++ recognizeFace.py
@@ -0,0 +1,15 @@
+# Match face and detect name.
+import cv2
+import os
+from time import sleep
+
+#Take image
+webcam = cv2.VideoCapture(0)
+check, frame = webcam.read()
+gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faces = detector.detectMultiScale(gray,1.1,5)
+
+
+cv2.imshow("image", frame)
+key = cv2.waitKey(1)
